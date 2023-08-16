69658d58b5528de852d199c721fae8fc3c3a07fc
Ben <baguirre@luc.edu>
2023-08-16 12:36:06 -0500

    place ship function in game is working

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -12,10 +12,53 @@ export default function Gameboard() {
         return holder;
     }
 
+    function placeShipIsOutOfBounds(shipObj, cordOne, cordTwo, direction) {
+        if (
+            direction === "horizontal" &&
+            cordOne - 1 + shipObj.getLength() > 10
+        ) {
+            return true;
+        }
+        if (
+            direction === "vertical" &&
+            cordTwo - 1 + shipObj.getLength() > 10
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    function findCords(shipObj, cordOne, cordTwo, direction) {
+        const newCords = [];
+        if (direction === "horizontal") {
+            newCords.push(cordTwo);
+            for (let i = 0; i <= shipObj.getLength(); i += 1) {
+                newCords.push(cordTwo + i);
+            }
+        }
+        if (direction === "vertical") {
+            newCords.push(cordOne);
+            for (let i = 0; i <= shipObj.getLength(); i += 1) {
+                newCords.push(cordOne + i);
+            }
+        }
+        return newCords;
+    }
+
     function placeShip(shipObj, cordOne, cordTwo, direction) {
-        if (shipIsOutOfBounds(shipObj, cordOne, cordTwo, direction)) {
+        if (placeShipIsOutOfBounds(shipObj, cordOne, cordTwo, direction)) {
             return false;
         }
+        const cords = findCords(shipObj, cordOne, cordTwo, direction);
+        cords.forEach((cord) => {
+            if (direction === "horizontal") {
+                this.board[cord][cordTwo] = shipObj;
+            }
+            if (direction === "vertical") {
+                this.board[cordOne][cord] = shipObj;
+            }
+        });
+        return this.board;
     }
 
     function getCords(cordOne, cordTwo) {
@@ -27,13 +70,4 @@ export default function Gameboard() {
         placeShip,
         getCords,
     };
-}
-
-function shipIsOutOfBounds(shipObj, cordOne, cordTwo, direction) {
-    if (direction === "horizontal" && cordOne - 1 + shipObj.getLength() > 10) {
-        return true;
-    }
-    if (direction === "vertical" && cordTwo - 1 + shipObj.getLength() > 10) {
-        return true;
-    }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,15 +4,19 @@ import Ship from "../modules/ship";
 
 const gameboard = new Gameboard();
 
-// test("place ship on gameboard", () => {
-//     gameboard.placeShip(1, 1, "test");
-//     expect(gameboard.board[1][1]).toEqual("test");
-// });
+test("place twoShip on gameboard", () => {
+    const twoShip = new Ship(2, "two-ship");
+    gameboard.placeShip(twoShip, 1, 1, "vertical");
+    expect(gameboard.board[1][1] && gameboard.board[1][2]).toEqual(twoShip);
+});
 
-// test("place ship in middle of board", () => {
-//     gameboard.placeShip(4, 5, "ship");
-//     expect(gameboard.board[4][5]).toEqual("ship");
-// });
+test("place twoShip on gameboard", () => {
+    const threeShip = new Ship(3, "two-ship");
+    gameboard.placeShip(threeShip, 2, 3, "horizontal");
+    expect(
+        gameboard.board[2][3] && gameboard.board[3][3] && gameboard.board[4][3],
+    ).toEqual(threeShip);
+});
 
 test("ship cannot be placed on edge of board vertical", () => {
     const threeShip = new Ship(3);
@@ -23,13 +27,3 @@ test("horizontal out of bounds test", () => {
     const fiveShip = new Ship(5);
     expect(gameboard.placeShip(fiveShip, 7, 5, "horizontal")).toEqual(false);
 });
-
-// test("place vertical of ship", () => {
-//     gameboard.placeShip(twoShip, 1, 1, "vertical");
-//     expect(gameboard.board[1][1] && gameboard.board[2][1]).toEqual("two-ship");
-// });
-
-// test("place horizontal ship", () => {
-//     gameboard.placeShip(twoShip, 2, 1, "horizontal");
-//     expect(gameboard.board[2][1] && gameboard.board[3][1]).toEqual("two-ship");
-// });
